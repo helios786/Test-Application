54710574c6adb4e6fd5f01732f78aeb2bfd1cf57
helios786 <ashvin@heliossolutions.in>
2010-09-03 20:47:49 +0530

    Commiting job-application code on behalf of Nikhilkumar Das

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nikhil::Application.config.secret_token = '4e8838f2edfd0bd64ffa1b266f89f47a98e7e6a1f2f220756458ef5921a91166ee16149d1930ecd51c04cd65109c34e05cdf0040d4cd0c63219ef04c504c9cee'
